53f5e3f9f1a8b4fcfa7c846432c37e9fceb8b2c4
Priyamani2004 <manimegalaulm@gmail.com>
2023-03-23 22:29:30 +0530

    new update

--- public/js/script.js
+++ public/js/script.js
@@ -14,48 +14,57 @@ let wholeElement=document.getElementById("text_contentdiv");
 //   }
 // });
 
-  function textcreate(element){
-    console.log(contentEditableDiv.childNodes)
-    let getid=Number(element.className);
-    console.log(getid)
-    let collectAlldiv=document.querySelectorAll('#writetext');
-    let textcontentlength=collectAlldiv.length;
-    //visible element function//
-    const visibleElements = [];
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.intersectionRatio > 0) {
-          visibleElements.push(entry.target);
-        }
-      });
-    });
+  // function textcreate(element){
+  //   console.log(contentEditableDiv.childNodes)
+  //   let getid=Number(element.className);
+  //   console.log(getid)
+  //   let collectAlldiv=document.querySelectorAll('#writetext');
+  //   let textcontentlength=collectAlldiv.length;
+  //   //visible element function//
+  //   const visibleElements = [];
+  //   const observer = new IntersectionObserver(entries => {
+  //     entries.forEach(entry => {
+  //       if (entry.intersectionRatio > 0) {
+  //         visibleElements.push(entry.target);
+  //       }
+  //     });
+  //   });
     
-    const allElements = document.querySelectorAll('*');
-    allElements.forEach(element => {
-      observer.observe(element);
-    });
-    console.log(visibleElements);
+  //   const allElements = document.querySelectorAll('*');
+  //   allElements.forEach(element => {
+  //     observer.observe(element);
+  //   });
+  //   console.log(visibleElements);
     
-      const selection = window.getSelection();
-      if (selection.rangeCount > 0) {
-        const range = selection.getRangeAt(0);
-        const rect = range.getBoundingClientRect();
-        console.log('Current position: ', rect.top);
-        if (rect.top > 700) {
-          if (getid === textcontentlength) {
-            const newPage = document.createElement("div");
-            newPage.contentEditable = true;
-            newPage.setAttribute("id", "writetext");
-            newPage.setAttribute("class", `${getid+1}`);
-            wholeElement.appendChild(newPage);
-            newPage.addEventListener("input", function() {
-              textcreate(newPage);
-            });
-          }
-        }
+  //     const selection = window.getSelection();
+  //     if (selection.rangeCount > 0) {
+  //       const range = selection.getRangeAt(0);
+  //       const rect = range.getBoundingClientRect();
+  //       console.log('Current position: ', rect.top);
+  //       if (rect.top > 700) {
+  //         if (getid === textcontentlength) {
+  //           const newPage = document.createElement("div");
+  //           newPage.contentEditable = true;
+  //           newPage.setAttribute("id", "writetext");
+  //           newPage.setAttribute("class", `${getid+1}`);
+  //           wholeElement.appendChild(newPage);
+  //           newPage.addEventListener("input", function() {
+  //             textcreate(newPage);
+  //           });
+  //         }
+  //       }
         
-      }
-    }
+  //     }
+  //   }
+
+//const editableDiv = document.getElementById("myContentEditableDiv");
+
+contentEditableDiv.addEventListener("mouseup", () => {
+  const selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+  const cursorPosition = range.startOffset;
+  console.log("Current cursor position:", cursorPosition);
+});
 
     // $(document).ready(function() {
     //   $("#contenteditable-div").on("blur", function() {
